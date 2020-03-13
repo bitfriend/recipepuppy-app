62e3acde1f477b52f688a3615848956433dbb412
Maksim <gorshkolepovm@gmail.com>
2020-03-14 01:43:15 +0300

    Implement axios

--- App.js
+++ App.js
@@ -9,20 +9,23 @@
 console.disableYellowBox = true;
 
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
-import { Card, SearchBar } from 'react-native-elements';
+import { ActivityIndicator, Dimensions, FlatList, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { Divider, SearchBar } from 'react-native-elements';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import qs from 'qs';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import axios from 'axios';
+
+const { width: windowWidth } = Dimensions.get('window');
+
+EStyleSheet.build({ $rem: windowWidth / 375 });
+
+const limit = 20;
 
 export default class App extends Component {
   state = {
     search: 'omelet',
-    json: {
-      title: '',
-      version: null,
-      href: '',
-      results: []
-    }
+    loading: false,
+    records: []
   }
 
   componentDidMount() {
@@ -30,52 +33,118 @@ export default class App extends Component {
   }
 
   doSearch(value) {
-    const data = {
-      i: 'onions,garlic',
-      q: value,
-      p: 3
-    };
-    fetch({
-      // url: 'http://www.recipepuppy.com/api/?' + qs.stringify(data, { encode: false }),
-      url: 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3',
-      method: 'GET'
-    }).then(response => {
-      console.log('response', response);
-      return response.json();
-    }).then(json => {
-      this.setState({ json });
+    this.setState({ loading: true });
+    this.doRequest(value, 1, []);
+  }
+
+  doRequest(value, page, records) {
+    axios({
+      url: 'http://www.recipepuppy.com/api/',
+      method: 'GET',
+      params: {
+        i: 'onions,garlic',
+        q: value,
+        p: page
+      }
+    }).then(res => {
+      const { results } = res.data;
+      if (results.length === 0) {
+        this.setState({ records, loading: false });
+        return;
+      }
+      for (let i = 0; i < results.length; i++) {
+        records.push(results[i]);
+        if (records.length === limit) {
+          this.setState({ records, loading: false });
+          return;
+        }
+      }
+      this.doRequest(value, page + 1, records);
     }).catch(error => {
       console.log('error', error.message);
+      this.setState({ loading: false });
     });
   }
 
-  onChangeKeyword = (value) => {
-    this.setState({ search: value });
-    this.doSearch(value);
+  onChangeText = (value) => {
+    if (!value) {
+      this.setState({
+        search: '',
+        records: []
+      });
+    } else {
+      this.setState({ search: value });
+      this.doSearch(value);
+    }
   }
 
   render = () => (
     <View style={styles.container}>
       <SearchBar
         value={this.state.search}
-        onChange={this.onChangeKeyword}
+        onChangeText={this.onChangeText}
       />
       <FlatList
-        data={this.state.json.results}
+        data={this.state.records}
         renderItem={this.renderItem}
+        ItemSeparatorComponent={() => <Divider />}
+        keyExtractor={(item, index) => index.toString()}
+        ListEmptyComponent={() => (
+          <View style={styles.emptyWrapper}>
+            <Text>Empty Data</Text>
+          </View>
+        )}
       />
+      {this.state.loading && (
+        <View style={styles.spinner}>
+          <ActivityIndicator size="large" />
+        </View>
+      )}
     </View>
   )
 
-  renderItem = ({ item, index }) => (
-    <Card title={item.title}>
-    </Card>
+  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <View style={styles.thumbnail}>
+        {!!item.thumbnail && (
+          <Image source={{ uri: item.thumbnail }} style={styles.thumbnail} />
+        )}
+      </View>
+      <Text style={styles.title}>{item.title.trim()}</Text>
+    </View>
   )
 }
 
-const styles = StyleSheet.create({
+const styles = EStyleSheet.create({
   container: {
     flex: 1,
     paddingTop: getStatusBarHeight(true)
+  },
+  spinner: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    justifyContent: 'center'
+  },
+  item: {
+    padding: '16rem',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  thumbnail: {
+    width: '40rem',
+    height: '40rem',
+    borderRadius: '5rem'
+  },
+  title: {
+    flex: 1,
+    marginLeft: '16rem'
+  },
+  emptyWrapper: {
+    flex: 1,
+    alignItems: 'center'
   }
 });
